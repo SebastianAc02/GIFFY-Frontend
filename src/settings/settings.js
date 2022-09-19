f3420713101b4aaad5a671d3d1099b2d5e779983
Sebastian Acosta <sacostamolina@outlook.com>
2022-09-18 17:01:34 -0700

    m

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -6,11 +6,9 @@ export const key = "rQTX6ScmQjCQOMr1xm1FBbMSTHRKO1MC";
 export const API_URL = `https://api.giphy.com/v1/gifs`
 
 
-<<<<<<< HEAD
-export const PRIVATE_API_URL = `http://localhost:400`
-=======
+
 export const PRIVATE_API_URL = `http://localhost:4003`
->>>>>>> origin/master
+
 
 
 
